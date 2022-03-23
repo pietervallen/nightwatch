01d6bf63a498aea5f8e4e5859ff1d85ac0c2a9be
Pieter Vallen <Pieter@Vallen.net>
2022-03-23 23:56:31 +0100

    Initial Commit

---
+++ NightWatch/NightWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  NightWatchApp.swift
+//  NightWatch
+//
+//  Created by Pieter Vallen on 23/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct NightWatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
